8d104053ba8e00ac55dca13e50d71e2b0b3c1c6e
Raphael Wegmueller <github@rofe.com>
2020-07-10 17:56:10 +0200

    test(smoke): test if images have loaded (#17)

--- test/smoke/test.post.content.output.js
+++ test/smoke/test.post.content.output.js
@@ -13,27 +13,51 @@
 /* eslint-disable no-console */
 
 const assert = require('assert');
+const puppeteer = require('puppeteer');
 const utils = require('./utils');
 
 const testDomain = process.env.TEST_DOMAIN;
 if (!testDomain) {
   throw new Error('Test domain missing, must be set by process.env.TEST_DOMAIN');
 }
-const url = `https://theblog-adobe.${testDomain}/en/publish/2020/03/19/introducing-public-beta.html`;
+const urlPrefix = `https://theblog-adobe.${testDomain}`;
+const url = `${urlPrefix}/en/publish/2020/03/19/introducing-public-beta.html`;
 
 describe(`Test theblog post page content for page ${url}`, () => {
-  let $;
 
-  // "function" is needed for "this", to set timeout
-  before(async function before() {
-    this.timeout(utils.HTTP_REQUEST_TIMEOUT_MSEC);
-    $ = await utils.getContentAs$(url);
-  });
-
-  it('contains the expected content', () => {
+  it('contains the expected content', async () => {
+    const $ = await utils.getContentAs$(url);
     assert.equal('Introducing Public Beta', $('title').text(), 'title');
     assert.equal(1, $('img[src="/hlx_20d6699b0815a3a7f4b124694d6a6ef556871cad.jpeg"]').length, 'hero banner');
     assert.equal(1, $('p:contains("by Nakiesha Koss")').length, 'author');
     assert.equal(1, $('p:contains("posted on 03-19-2020")').length, 'posted on date');
-  });
+  }).timeout(utils.HTTP_REQUEST_TIMEOUT_MSEC);
+
+  it('makes the expected requests', async () => {
+    let numReqs = 0;
+    const failedReqs = [];
+    const wrongImgs = [];
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    page.on('response', (resp) => {
+      numReqs += 1;
+      // test response of blog requests
+      if (!resp.ok() && resp.url().startsWith(urlPrefix)) {
+        failedReqs.push(`${resp.url()}: ${resp.status()}`);
+      }
+      // test content types of hlx images
+      if (/.*\/hlx_.*(jpeg|jpg|png|gif|webp).*/.test(resp.url())
+        && !resp.headers()['content-type'].startsWith('image/')) {
+        wrongImgs.push(`${resp.url()}: ${resp.headers()['content-type']}`);
+      }
+    });
+    await page.goto(url, { waitUntil: 'networkidle2' });
+    browser.close();
+    // test number of requests
+    assert.ok(numReqs, 'browser made requests');
+    // no failed requests
+    assert.equal(0, failedReqs.length, `The following request(s) failed:\n${failedReqs.join('\n')}`);
+    // no wrong image content types
+    assert.equal(0, wrongImgs.length, `The following image(s) had an invalid content type:\n${wrongImgs.join('\n')}`);
+  }).timeout(utils.HTTP_REQUEST_TIMEOUT_MSEC);
 });
