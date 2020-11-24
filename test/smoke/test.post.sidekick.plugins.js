af455bf907b20f8d3bd1e0b1cd8398cd95c9d92f
rofe <github@rofe.com>
2020-11-24 15:31:00 +0100

    chore: remove unused global

--- test/smoke/test.post.sidekick.plugins.js
+++ test/smoke/test.post.sidekick.plugins.js
@@ -10,7 +10,7 @@
  * governing permissions and limitations under the License.
  */
 /* eslint-env mocha */
-/* global window document navigator */
+/* global window document */
 
 const assert = require('assert');
 const puppeteer = require('puppeteer');
