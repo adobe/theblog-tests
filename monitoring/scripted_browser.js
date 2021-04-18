83d15fdfd496670ee66583e55475bb3ce303518b
rofe <github@rofe.com>
2021-04-18 22:19:09 +0200

    feat(monitoring): fail if static marker present

--- monitoring/scripted_browser.js
+++ monitoring/scripted_browser.js
@@ -27,8 +27,17 @@ const baseUrl = '$$$URL$$$';
 async function checkHomepage(url) {
   $browser.get(url)
     // Get articles
-    .then(() => {
-      console.log(`Page ${url} loaded. Waiting now for '.load-more' element...`);
+    .then(async () => {
+      console.log(`Page ${url} loaded.`);
+      // First make sure the homepage is not static
+      let staticMarker;
+      try {
+        staticMarker = await $browser.waitForAndFindElement($driver.By.id('___WARNING__STATIC_HOMEPAGE___'), 5000);
+      } catch (e) {
+        console.log('This homepage is static: backend must be down!');
+      }
+      assert.ok(!staticMarker);
+      console.log('Waiting now for ".load-more" element...');
       return $browser.waitForAndFindElement($driver.By.css('.load-more'), 60000);
     }).then(() => {
       console.log('\'.load-more\' found. Retrieving the articles...');
